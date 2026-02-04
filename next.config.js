9bdc7daedb494d53d28cce6484ef6cec91f96e6c
betogomes <rgomes.silva@gmail.com>
2026-02-04 11:34:49 -0300

    Fix: Corrigir exportação da página principal

--- next.config.js
+++ next.config.js
@@ -7,3 +7,14 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
